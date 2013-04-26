895a205afa2666418da7a6b4cc2074a8eb92d6b2
Rob Lund <roblund@y7mail.com>
2013-04-26 22:11:58 +1000

    Added status validations and tests

--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+  test "that a status requires content" do
+  	status = Status.new
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+ 	test "that a status's content is atleast 30 letters" do
+ 		status = Status.new
+ 		status.content = "Hello this is 27 characters"
+  		assert !status.save
+  		assert !status.errors[:content].empty?
+  end
+
+  test "that a status has a user id" do
+  	status = Status.new
+ 	status.content = "Hello this is more than 30 characters"
+  	assert !status.save
+  	assert !status.errors[:user_id].empty?
+	end
+ end
+
+ 
